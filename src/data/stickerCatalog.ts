12603de3ca59e0ebd600a64b693ad4ca0e5d3bf1
shengwenjin <jsw504083881@gmail.com>
2026-03-17 10:59:55 +0800

    update stickers

--- src/data/stickerCatalog.ts
+++ src/data/stickerCatalog.ts
@@ -17,8 +17,28 @@ export const STICKER_CATALOG: StickerCategoryDefinition[] = [
     stickers: [
       {
         id: 'cat1',
-        src: '/stckers/cat/cat1.png',
+        src: '/stickers/cat/cat1.png',
         label: 'Cat 1',
+      },
+      {
+        id: 'cat2',
+        src: '/stickers/cat/cat2.png',
+        label: 'Cat 2',
+      },
+      {
+        id: 'cat3',
+        src: '/stickers/cat/cat3.png',
+        label: 'Cat 3',
+      },
+      {
+        id: 'cat4',
+        src: '/stickers/cat/cat4.png',
+        label: 'Cat 4',
+      },
+      {
+        id: 'cat5',
+        src: '/stickers/cat/cat5.png',
+        label: 'Cat 5',
       },
     ],
   },
